a108fc99ea2b163c41e42b325d027fcd606ea57a
Andi <andixtoel@gmail.com>
2024-12-19 08:59:06 +0700

    update: starting for daily

--- app/src/main/java/com/ndirawan/daily/MainActivity.kt
+++ app/src/main/java/com/ndirawan/daily/MainActivity.kt
@@ -4,23 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.ndirawan.daily.data.remote.ApiService
-import com.ndirawan.daily.data.repository.impl.TodosRepositoryImpl
-import com.ndirawan.daily.ui.screen.detailTodo.view.TodosScreen
-import com.ndirawan.daily.ui.screen.detailTodo.viewModel.TodosViewModel
-import com.ndirawan.daily.ui.screen.detailTodo.viewModel.TodosViewModelFactory
-import com.ndirawan.daily.ui.theme.DailyTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            DailyTheme {
-                val vm: TodosViewModel = viewModel(factory = TodosViewModelFactory(TodosRepositoryImpl(ApiService.client)))
-                TodosScreen(vm)
-            }
+            
         }
     }
 }
--- app/src/main/java/com/ndirawan/daily/data/repository/TodosRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.ndirawan.daily.data.repository
-
-import com.ndirawan.daily.model.Todos
-
-interface TodosRepository {
-    suspend fun getTodos(): Todos
-}
